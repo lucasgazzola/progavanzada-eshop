24f749e957ac741b7a00dbc4cec1bfaf6669eec2
Lucas Gazzola <lucasgazzola1@outlook.com>
2024-09-26 14:43:03 -0300

    permitir marcas de entre 2 y 32 caracteres

--- src/main/java/com/eshop/progavanzada/dtos/MarcaDTO.java
+++ src/main/java/com/eshop/progavanzada/dtos/MarcaDTO.java
@@ -3,6 +3,7 @@ package com.eshop.progavanzada.dtos;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,6 +12,7 @@ public class MarcaDTO {
   @Positive(message = "El id debe ser positivo")
   private Integer id;
 
+  @Size(min = 2, max = 32, message = "El nombre debe tener entre 2 y 32 caracteres.")
   @NotNull(message = "El nombre es obligatorio")
   @NotEmpty(message = "El nombre no puede estar vacío")
   private String nombre;
