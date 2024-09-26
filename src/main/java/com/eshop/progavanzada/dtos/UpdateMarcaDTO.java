c9468faac9717e426e3a2f0796e1e79732a69437
Lucas Gazzola <lucasgazzola1@outlook.com>
2024-09-26 14:41:01 -0300

    descripcion no es obligatoria

--- src/main/java/com/eshop/progavanzada/dtos/UpdateMarcaDTO.java
+++ src/main/java/com/eshop/progavanzada/dtos/UpdateMarcaDTO.java
@@ -2,6 +2,8 @@ package com.eshop.progavanzada.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,9 +11,10 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateMarcaDTO {
   @Size(min = 2, max = 32, message = "El nombre debe tener entre 2 y 32 caracteres.")
+  @NotNull(message = "El nombre es obligatorio")
+  @NotEmpty(message = "El nombre no puede estar vacío")
   private String nombre;
 
-  @Size(min = 1, max = 128, message = "La descripción debe tener entre 1 y 128 caracteres.")
   private String descripcion;
 
   public boolean isEmpty() {
