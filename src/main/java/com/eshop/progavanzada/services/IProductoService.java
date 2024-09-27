003f9a8a31c5c123eb9550672ba585a39a338624
Lucas Gazzola <lucasgazzola1@outlook.com>
2024-09-27 14:06:35 -0300

    cambio interfaz de productoservice

--- src/main/java/com/eshop/progavanzada/services/IProductoService.java
+++ src/main/java/com/eshop/progavanzada/services/IProductoService.java
@@ -5,15 +5,21 @@ import jakarta.persistence.ElementCollection;
 import java.util.List;
 
 import com.eshop.progavanzada.dtos.ProductoDTO;
+import com.eshop.progavanzada.dtos.UpdateProductoDTO;
+import com.eshop.progavanzada.models.Producto;
 
 public interface IProductoService {
 
   @ElementCollection(targetClass = Integer.class)
-  public List<ProductoDTO> listar();
+  public List<ProductoDTO> listarProductos();
 
   public ProductoDTO buscarPorId(Integer id);
 
-  public ProductoDTO guardar(ProductoDTO productoDTO);
+  public ProductoDTO crearProducto(ProductoDTO marcaDTO);
 
-  public void eliminar(Integer id);
+  public ProductoDTO actualizarProducto(Integer id, UpdateProductoDTO marcaDTO);
+
+  public void eliminarProducto(Integer id);
+
+  public void recuperarProducto(Producto marca);
 }
