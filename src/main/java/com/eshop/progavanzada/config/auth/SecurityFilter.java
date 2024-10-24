ecedbd94343d28ac39d6743aa2719755a36b62e8
Lucas Gazzola <lucasgazzola1@outlook.com>
2024-10-24 20:30:30 -0300

    eliminados imports no utilizados

--- src/main/java/com/eshop/progavanzada/config/auth/SecurityFilter.java
+++ src/main/java/com/eshop/progavanzada/config/auth/SecurityFilter.java
@@ -2,17 +2,13 @@ package com.eshop.progavanzada.config.auth;
 
 import java.io.IOException;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.eshop.progavanzada.models.User;
 import com.eshop.progavanzada.repositories.UserRepository;
 
 import jakarta.servlet.FilterChain;
